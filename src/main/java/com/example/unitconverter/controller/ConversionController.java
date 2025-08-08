5fa40267b211ecd608e8d5316c9d1391dc2bbdca
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-08 12:50:18 -0600

    display result

--- src/main/java/com/example/unitconverter/controller/ConversionController.java
+++ src/main/java/com/example/unitconverter/controller/ConversionController.java
@@ -1,18 +1,33 @@
 package com.example.unitconverter.controller;
 
+import com.example.unitconverter.service.UnitConversionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ConversionController {
-    @GetMapping ("/")
-    public String home () {
-        return "index";
+//    @GetMapping ("/")
+//    public String home () {
+//        return "index";
+//    }
+
+    private UnitConversionService conversionService;
+
+    public ConversionController (UnitConversionService conversionService) {
+        this.conversionService = conversionService;
     }
 
     @GetMapping ("/convert")
-    public String convert (@RequestParam double value, @RequestParam String from, @RequestParam String to) {
-        return "";
+    public String convert (@RequestParam double amount, @RequestParam String from, @RequestParam String to, RedirectAttributes redirect) {
+        double result = this.conversionService.convert (amount, from, to);
+
+        redirect.addAttribute ("amount", amount);
+        redirect.addAttribute ("from", from);
+        redirect.addAttribute ("to", to);
+        redirect.addAttribute ("result", result);
+
+        return "redirect:/";
     }
 }
