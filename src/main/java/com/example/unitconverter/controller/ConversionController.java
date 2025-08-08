4ab8f203353d4a3164891a2f866f49ccf47388a2
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-08 10:37:38 -0600

    ConversionController

--- src/main/java/com/example/unitconverter/controller/ConversionController.java
+++ src/main/java/com/example/unitconverter/controller/ConversionController.java
@@ -1,4 +1,12 @@
 package com.example.unitconverter.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ConversionController {
+    @GetMapping ("/")
+    public String home () {
+        return "index";
+    }
 }
