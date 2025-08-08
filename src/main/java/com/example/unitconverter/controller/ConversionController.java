06abf6d5aab6c0b083c3201f89052bca99a3cf05
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-08 11:46:31 -0600

    Unit class

--- src/main/java/com/example/unitconverter/controller/ConversionController.java
+++ src/main/java/com/example/unitconverter/controller/ConversionController.java
@@ -2,11 +2,17 @@ package com.example.unitconverter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ConversionController {
     @GetMapping ("/")
     public String home () {
         return "index";
+    }
+
+    @GetMapping ("/convert")
+    public String convert (@RequestParam double value, @RequestParam String from, @RequestParam String to) {
+        return "";
     }
 }
--- src/main/java/com/example/unitconverter/model/UnitType.java
+++ src/main/java/com/example/unitconverter/model/UnitType.java
@@ -1,4 +1,18 @@
 package com.example.unitconverter.model;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class UnitType {
+    private double amount;
+    private String symbol;
+
+    public UnitType (double amount, String symbol) {
+        this.amount = amount;
+        this.symbol = symbol;
+    }
+
+    public String toString () {
+        return this.amount + " " + this.symbol;
+    }
 }
