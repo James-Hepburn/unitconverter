8a15eee75e45f9617352bec5ecacad5082b183ef
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-08 11:51:22 -0600

    ConversionService

--- src/main/java/com/example/unitconverter/service/UnitConversionService.java
+++ src/main/java/com/example/unitconverter/service/UnitConversionService.java
@@ -1,4 +1,10 @@
 package com.example.unitconverter.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UnitConversionService {
+    public double convert (double amount, String from, String to) {
+        return 0;
+    }
 }
